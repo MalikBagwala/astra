f27a5fbe5999cecf502fd5b86f220927a7eebb14
MalikBagwala <m.bagwala@gmail.com>
2024-03-15 14:35:42 +0530

    chore: add basic modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,39 @@
+import { useEffect } from "react";
 import styles from "./Modal.module.css";
 type Props = {
   isOpen?: boolean;
   onClose?: () => void;
+  children?: React.ReactNode;
 };
 
-const Modal = ({}: Props) => {
-  return <div className={styles.base}>Modal</div>;
+const Modal = ({ isOpen = true, onClose, children }: Props) => {
+  function handleClose() {
+    onClose?.();
+  }
+
+  useEffect(() => {
+    const body = document.getElementsByTagName("body")[0];
+    if (isOpen) {
+      body.classList.add("overflow-hidden");
+    } else {
+      body.classList.remove("overflow-hidden");
+    }
+    return () => {
+      body.classList.remove("overflow-hidden");
+    };
+  }, [isOpen]);
+  if (!isOpen) return null;
+  return (
+    <>
+      <div className={styles.overlay} />
+      <div className={styles.body}>
+        <button className={styles.close} onClick={handleClose}>
+          X
+        </button>
+        <div className={styles.content}>{children}</div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
